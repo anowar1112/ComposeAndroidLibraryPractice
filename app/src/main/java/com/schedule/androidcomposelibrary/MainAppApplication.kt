82611025dbb85c4ffedc2b30a4872cf5d300159e
Anowar Hossain <anowar1112@gmail.com>
2025-09-06 21:56:50 +0600

    [Title] added main application file

---
+++ app/src/main/java/com/schedule/androidcomposelibrary/MainAppApplication.kt
@@ -0,0 +1,9 @@
+package com.schedule.androidcomposelibrary
+
+import android.app.Application
+
+class MainAppApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
